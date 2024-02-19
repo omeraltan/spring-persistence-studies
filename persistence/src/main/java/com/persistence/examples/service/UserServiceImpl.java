c38c0a3e6d42fb0df402a75a8241cf7ac38aa3b2
omeraltan <omeraltan66@gmail.com>
2024-02-19 11:07:15 +0300

    Commit All

---
+++ persistence/src/main/java/com/persistence/examples/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.persistence.examples.service;
+
+import com.persistence.examples.entity.User;
+import com.persistence.examples.util.SearchCriteria;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserServiceImpl implements UserService{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> searchUser(final List<SearchCriteria> params) {
+        final CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        final CriteriaQuery<User> query = builder.createQuery(User.class);
+        final Root r = query.from(User.class);
+
+        Predicate predicate = builder.conjunction();
+        UserSearchQueryCriteriaConsumer searchConsumer = new UserSearchQueryCriteriaConsumer(predicate, builder, r);
+        params.stream().forEach(searchConsumer);
+        predicate = searchConsumer.getPredicate();
+        query.where(predicate);
+
+        return entityManager.createQuery(query).getResultList();
+    }
+
+    @Override
+    public void save(User entity) {
+        entityManager.persist(entity);
+    }
+}
